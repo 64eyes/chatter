96455d0896d2bddabf4c8d47b1d83610e5d91ead
Madamori Oloruntobi <madamorit8@gmail.com>
2024-06-14 04:32:17 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.css'],
+      rules: {
+        'tailwindcss/no-custom-classname': 'off',
+      },
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>Hello Chatter</>;
+}
+
+export default App;
